7ce2146960ebba7d6566b22c57b4d9ad5e7d7ea4
Flórián Morvai <fmorvai@gmail.com>
2016-05-17 22:48:43 +0200

    user backend

---
+++ src/main/java/hu/szte/prf/taskmanager/service/UserService.java
@@ -0,0 +1,15 @@
+package hu.szte.prf.taskmanager.service;
+
+import java.util.List;
+
+import hu.szte.prf.taskmanager.entity.User;
+
+public interface UserService {
+	
+	public User find(final Long id);
+	
+	public List<User> list();
+	
+	public void save(final User user);
+
+}
